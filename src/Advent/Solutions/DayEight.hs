6650f1ce6eb1000345b70a0893782481f15e15a7
Daem0n-th <dheemanthmanur72@gmail.com>
2021-12-09 22:40:21 +0530

    Add type signatures

--- src/Advent/Solutions/DayEight.hs
+++ src/Advent/Solutions/DayEight.hs
@@ -1,6 +1,6 @@
 module Advent.Solutions.DayEight where
 
-import Data.List
+import Data.List (sort, (\\))
 import Data.List.Split (splitOn)
 
 dayEight :: IO ()
@@ -15,13 +15,16 @@ partOne = length . filter (`elem` n) . digits
     digits = concatMap (map length . words . last . splitOn " | ")
     n = [2, 4, 3, 7]
 
+partTwo :: [String] -> Int
 partTwo = sum . map decode
 
+decode :: String -> Int
 decode x = foldl (\acc x -> acc * 10 + x) 0 $ map (getKey digitMap . sort) $ words $ head b
   where
     (a : b) = splitOn " | " x
     digitMap = createMap $ map sort $ words a
 
+createMap :: [String] -> [(Int, String)]
 createMap xs = bm ++ map (cat5 bm) (onlyLen 5) ++ map (cat6 bm) (onlyLen 6)
   where
     onlyLen a = filter (\x -> length x == a) xs
@@ -32,6 +35,7 @@ baseMap xs = map (func xs) [(1, 2), (4, 4), (7, 3), (8, 7)]
   where
     func xs (a, b) = (a, head $ filter (\x -> length x == b) xs)
 
+cat5 :: [(Int, String)] -> String -> (Int, String)
 cat5 bm x
   | get 1 `isIn` x = (3, x)
   | (get 4 \\ get 1) `isIn` x = (5, x)
@@ -39,6 +43,7 @@ cat5 bm x
   where
     get = getEle bm
 
+cat6 :: [(Int, String)] -> String -> (Int, String)
 cat6 bm x
   | (get 8 \\ get 1) `isIn` x = (6, x)
   | get 4 `isIn` x = (9, x)
@@ -46,8 +51,11 @@ cat6 bm x
   where
     get = getEle bm
 
+getEle :: Eq a => [(a, b)] -> a -> b
 getEle xs x = snd $ head $ filter (\(a, _) -> a == x) xs
 
+getKey :: Eq b => [(a, b)] -> b -> a
 getKey xs x = fst $ head $ filter (\(_, b) -> b == x) xs
 
+isIn :: String -> String -> Bool
 isIn a b = all (`elem` b) a
